2f70b5bd9290c2c384a193d13fcbd1c951b06f7a
nerolliu <nerolliu@tencent.com>
2024-05-16 23:47:03 +0800

    强制高刷

--- android/app/src/main/kotlin/com/example/performance_test/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/performance_test/MainActivity.kt
@@ -4,13 +4,18 @@ import android.graphics.Point
 import android.os.Build
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.view.Display
+import android.view.Surface
+import android.view.SurfaceHolder
+import androidx.annotation.RequiresApi
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterSurfaceView
 
 class MainActivity: FlutterActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         trySetHighRefreshRate()
     }
 
@@ -18,15 +23,19 @@ class MainActivity: FlutterActivity() {
         if (activity == null) return
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             val supportDisplayMode = activity.display?.supportedModes ?: run {
+                Log.i("nero", "cannot get support display mode")
                 return
             }
+            Log.i("nero", "support display mode size:${supportDisplayMode.size}")
             var maxRefreshRateMode: Display.Mode? = null
             val resolution = Point()
             activity.display?.getRealSize(resolution)
 
             //获取当前的刷新率
             val currentReFreshRate = activity.display?.mode?.refreshRate
+            Log.i("nero", "current refreshRate:$currentReFreshRate")
             for (mode in supportDisplayMode) {
+                Log.i("nero", "refreshRate:${mode.refreshRate}")
 //                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
 //                    Log.i(TAG, "alternativeRefreshRates:${mode.alternativeRefreshRates.joinToString(",")}")
 //                }
@@ -40,6 +49,7 @@ class MainActivity: FlutterActivity() {
                 val lp = activity.window.attributes
 
                 if (currentReFreshRate == null || it.refreshRate > currentReFreshRate) {
+                    Log.i("nero", "refresh rate diff, try set:${it.refreshRate}")
                     lp.preferredDisplayModeId = it.modeId
                     activity.window.attributes = lp
                 }
